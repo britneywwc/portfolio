e57914722686c5cd16c6973ddc43e89110ec5334
britneywwc <britneywwc@gmail.com>
2023-04-23 22:36:13 +1000

    Fixed indenting

--- src/components/CurvedText/index.js
+++ src/components/CurvedText/index.js
@@ -5,22 +5,22 @@ import ReactCurvedText from 'react-curved-text';
 const CurvedText = () => {
     return (
         <div className="curvedtext">
-        <ReactCurvedText width='147'
-  height='156'
-  cx='178'
-  cy='196'
-  rx='160'
-  ry='105'
-  startOffset='43'
-  reversed={true}
-  text='britney wang'
-  textProps={{"style": {"fontSize": "22"}}}
-  textPathProps={null}
-  tspanProps={null}
-  ellipseProps={null}
-  svgProps={null} />
+            <ReactCurvedText width='147'
+                height='156'
+                cx='178'
+                cy='196'
+                rx='160'
+                ry='105'
+                startOffset='43'
+                reversed={true}
+                text='britney wang'
+                textProps={{"style": {"fontSize": "22"}}}
+                textPathProps={null}
+                tspanProps={null}
+                ellipseProps={null}
+                svgProps={null} />
 
-  </div>
+        </div>
 
     )
 }
