549c9a4c2a595ee2aa32a0b2bec89e270157d02f
britneywwc <britneywwc@gmail.com>
2023-08-22 20:14:38 +0800

    Used CardMedia for ProjectScroll section

--- src/components/ProjectScroll/index.js
+++ src/components/ProjectScroll/index.js
@@ -1,12 +1,18 @@
 import React from "react"
 import { useMediaQuery } from '@mui/material';
+import {CardMedia} from "@mui/material";
 
 const ProjectScroll = props => {
     const images = props.images;
     const appType = props.appType;
     const mobileView = useMediaQuery('(max-width:800px)');
 
+    const portraitView = window.innerHeight > window.innerWidth ? true : false;
+    console.log('h:', window.innerHeight, 'w:', window.innerWidth, portraitView);
+
     var imgHeight, imgWidth;
+
+ 
 
     if (appType === "web") {
         if (mobileView) {
@@ -21,26 +27,27 @@ const ProjectScroll = props => {
             imgHeight = '60vh';
             imgWidth = '70vw';
         } else {
-            imgHeight = '50vw';
+            imgHeight = '40vw';
             imgWidth = '50vh';
         }
-    }
-    
-    
+    }    
 
     return (
         <div className="project__container">
-            {images.map(image => (
-                <div 
-                    key={image}
-                    className="project__card"
-                    style=
-                    {{
-                        backgroundImage: `url(${image})`,
+            {images.map(image => (                
+                <CardMedia
+                    component="img"
+                    height="100%"
+                    image={image}
+                    alt="project image"
+                    sx={{
                         width: `${imgWidth}`,
                         height: `${imgHeight}`,
-                    }}
-                />
+                        borderRadius: '5px',
+                        margin: '.5em',
+                    }}     
+                                               
+                /> 
             ))}
         </div>
     )
